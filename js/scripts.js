2ca0561b1736ac4aafd1cb7e4326ff3897dbe0bb
onurkaymak <onurkaymak@outlook.com>
2023-08-22 14:32:41 -0700

    Pass Test 5
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -19,3 +19,14 @@ function Account(name, initialDeposit) {
     this.balance = initialDeposit;
 };
 
+Account.prototype.depWith = function (trans, amount) {
+    if (trans === 'deposit') {
+        account.balance += amount;
+    } else if (trans === 'withdraw') {
+        account.balance -= amount;
+    }
+}
+
+// const account = new Account("Onur", 5000);
+// const bank = new BankAccounts();
+// bank.addAccount(account);
