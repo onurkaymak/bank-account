9913ce89cf8dc9f1569ef474d89a86b33951918b
onurkaymak <onurkaymak@outlook.com>
2023-08-22 13:32:51 -0700

    Pass Test 4
    Co-authored-by: Paul LeTourneau <pletourneau62@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,18 @@
 function BankAccounts() {
     this.accounts = {};
+    this.currentId = 0;
+};
+
+BankAccounts.prototype.addAccount = function (account) {
+    account.id = this.assignId();
+    this.accounts[account.id] = account;
 };
 
 
-
+BankAccounts.prototype.assignId = function () {
+    this.currentId += 1;
+    return this.currentId;
+};
 
 function Account(name, initialDeposit) {
     this.name = name;
